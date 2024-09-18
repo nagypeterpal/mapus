1e676491ea61a1fa4c69d49f7f3abd14ad7b07a2
nagypeterpal <nagypeterpal@gmail.com>
2024-09-18 11:26:01 +0200

    qwe

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-# todos-express-google
-
-This app illustrates how to use [Passport](https://www.passportjs.org/) with
-[Express](https://expressjs.com/) to sign users in with [Google](https://www.google.com/).
-Use this example as a starting point for your own web applications.
+# mapus
 
 ## Quick Start
 
 To run this app, clone the repository and install dependencies:
 
 ```bash
-$ git clone https://github.com/passport/todos-express-google.git
+$ git clone https://github.com/passport/mapus.git
 $ cd todos-express-google
 $ npm install
 ```
@@ -35,56 +31,8 @@ $ npm start
 
 Navigate to [`http://localhost:3000`](http://localhost:3000).
 
-## Tutorial
-
-Follow along with the step-by-step [Sign In with Google Tutorial](https://www.passportjs.org/tutorials/google/)
-to learn how this app was built.
-
-## Overview
-
-This app illustrates how to build a todo app with sign in functionality using
-Express, Passport, and the [`passport-google-oidc`](https://www.passportjs.org/packages/passport-google-oidc/)
-strategy.
-
-This app is a traditional web application, in which application logic and data
-persistence resides on the server.  HTML pages and forms are rendered by the
-server and client-side JavaScript is not utilized (or kept to a minimum).
-
-This app is built using the Express web framework.  Data is persisted to a
-[SQLite](https://www.sqlite.org/) database.  HTML pages are rendered using [EJS](https://ejs.co/)
-templates, and are styled using vanilla CSS.
-
-When a user first arrives at this app, they are prompted to sign in.  To sign
-in, the user is redirected to Google using OpenID Connect.  Once authenticated,
-a login session is established and maintained between the server and the user's
-browser with a cookie.
-
-After signing in, the user can view, create, and edit todo items.  Interaction
-occurs by clicking links and submitting forms, which trigger HTTP requests.
-The browser automatically includes the cookie set during login with each of
-these requests.
-
-When the server receives a request, it authenticates the cookie and restores the
-login session, thus authenticating the user.  It then accesses or stores records
-in the database associated with the authenticated user.
-
-## Next Steps
-
-* Add additional social login providers.
-
-  Study [todos-express-social](https://github.com/passport/todos-express-social)
-  to learn how to add multiple providers, giving users the choice of which
-  social network account to use when signing in.
-
-* Add passwordless.
-
-  Study [todos-express-webauthn](https://github.com/passport/todos-express-webauthn)
-  to learn how to let users sign in with biometrics or a security key.
-
-## License
-
 [The Unlicense](https://opensource.org/licenses/unlicense)
 
 ## Credit
 
-Created by [Jared Hanson](https://www.jaredhanson.me/)
+Created by Nagy Peter
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oidc');
-var db = require('../db');
-
+var express = require("express");
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oidc");
+var db = require("../db");
 
 // Configure the Google strategy for use by Passport.
 //
@@ -11,46 +10,65 @@ var db = require('../db');
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
-passport.use(new GoogleStrategy({
-  clientID: process.env['GOOGLE_CLIENT_ID'],
-  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-  callbackURL: '/oauth2/redirect/google',
-  scope: [ 'profile' ]
-}, function verify(issuer, profile, cb) {
-  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-    issuer,
-    profile.id
-  ], function(err, row) {
-    if (err) { return cb(err); }
-    if (!row) {
-      db.run('INSERT INTO users (name) VALUES (?)', [
-        profile.displayName
-      ], function(err) {
-        if (err) { return cb(err); }
-        var id = this.lastID;
-        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-          id,
-          issuer,
-          profile.id
-        ], function(err) {
-          if (err) { return cb(err); }
-          var user = {
-            id: id,
-            name: profile.displayName
-          };
-          return cb(null, user);
-        });
-      });
-    } else {
-      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
-        if (err) { return cb(err); }
-        if (!row) { return cb(null, false); }
-        return cb(null, row);
-      });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env["GOOGLE_CLIENT_ID"],
+      clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
+      callbackURL: "/oauth2/redirect/google",
+      scope: ["profile"],
+    },
+    function verify(issuer, profile, cb) {
+      db.get(
+        "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
+        [issuer, profile.id],
+        function (err, row) {
+          if (err) {
+            return cb(err);
+          }
+          if (!row) {
+            db.run(
+              "INSERT INTO users (name) VALUES (?)",
+              [profile.displayName],
+              function (err) {
+                if (err) {
+                  return cb(err);
+                }
+                var id = this.lastID;
+                db.run(
+                  "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
+                  [id, issuer, profile.id],
+                  function (err) {
+                    if (err) {
+                      return cb(err);
+                    }
+                    var user = { id: id, name: profile.displayName };
+                    return cb(null, user);
+                  }
+                );
+              }
+            );
+          } else {
+            db.get(
+              "SELECT * FROM users WHERE id = ?",
+              [row.user_id],
+              function (err, row) {
+                if (err) {
+                  return cb(err);
+                }
+                if (!row) {
+                  return cb(null, false);
+                }
+                return cb(null, row);
+              }
+            );
+          }
+        }
+      );
     }
-  });
-}));
-  
+  )
+);
+
 // Configure Passport authenticated session persistence.
 //
 // In order to restore authentication state across HTTP requests, Passport needs
@@ -60,18 +78,17 @@ passport.use(new GoogleStrategy({
 // from the database when deserializing.  However, due to the fact that this
 // example does not have a database, the complete Facebook profile is serialized
 // and deserialized.
-passport.serializeUser(function(user, cb) {
-  process.nextTick(function() {
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
     cb(null, { id: user.id, username: user.username, name: user.name });
   });
 });
 
-passport.deserializeUser(function(user, cb) {
-  process.nextTick(function() {
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
     return cb(null, user);
   });
 });
-
 
 var router = express.Router();
 
@@ -83,8 +100,8 @@ var router = express.Router();
  * user to sign in with Google.  When the user clicks this button, a request
  * will be sent to the `GET /login/federated/accounts.google.com` route.
  */
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.get("/login", function (req, res, next) {
+  res.render("login");
 });
 
 /* GET /login/federated/accounts.google.com
@@ -99,7 +116,7 @@ router.get('/login', function(req, res, next) {
  * Once Google has completed their interaction with the user, the user will be
  * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
  */
-router.get('/login/federated/google', passport.authenticate('google'));
+router.get("/login/federated/google", passport.authenticate("google"));
 
 /*
     This route completes the authentication sequence when Google redirects the
@@ -107,19 +124,24 @@ router.get('/login/federated/google', passport.authenticate('google'));
     automatically created and their Google account is linked.  When an existing
     user returns, they are signed in to their linked account.
 */
-router.get('/oauth2/redirect/google', passport.authenticate('google', {
-  successReturnToOrRedirect: '/',
-  failureRedirect: '/login'
-}));
+router.get(
+  "/oauth2/redirect/google",
+  passport.authenticate("google", {
+    successReturnToOrRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
 /* POST /logout
  *
  * This route logs the user out.
  */
-router.post('/logout', function(req, res, next) {
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
 });
 
